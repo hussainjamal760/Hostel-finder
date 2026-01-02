e84c0b1dd9fbfcddd26d5a17f54e81a1290be694
Hussain Jamal <Hjamal9865@gmail.com>
2026-01-03 02:03:17 +0500

    user model added

---
+++ server/models/user.model.ts
@@ -0,0 +1,87 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+import * as bcrypt from "bcryptjs";
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    phone:string;
+    avatar: {
+        public_id: string;
+        url: string;
+    };
+    role: "user" | "manager" | "admin";
+    isActive:boolean;
+    hostel?: mongoose.Types.ObjectId;
+    hostelRequestStatus: "none" | "pending" | "approved" | "rejected";
+    comparePassword: (password: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        lowercase: true,
+        trim: true,
+        unique: true,
+        validate: {
+            validator: (value: string) => emailRegexPattern.test(value),
+            message: "Please enter a valid email"
+        }
+    },
+    password: {
+        type: String,
+        minlength: [6, "Password must be atleast 6 characters"],
+        select: false,
+    },
+   phone: {
+     type: String,
+     required: true,
+     trim: true,
+     match: [/^[0-9]{10,15}$/, "Please enter a valid phone number"]
+},
+    avatar: {
+        public_id: String,
+        url: String,
+    },
+    role: {
+        type: String,
+        enum: ["user", "manager", "admin"],
+        default: "user"
+    },
+    isActive: {
+    type: Boolean,
+    default: true
+    },
+    hostel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hostel"
+    },
+    hostelRequestStatus: {
+    type: String,
+    enum: ["none", "pending", "approved", "rejected"],
+    default: "none"
+    }
+
+}, { timestamps: true });
+
+userSchema.pre<IUser>('save', async function () {
+    if (!this.isModified('password')) return;
+
+    this.password = await bcrypt.hash(this.password, 10);
+});
+
+
+userSchema.methods.comparePassword = async function(enteredPassword: string): Promise<boolean> {
+    if (!this.password) return false;
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const userModel: Model<IUser> = mongoose.model<IUser>("User", userSchema);
+export default userModel;
