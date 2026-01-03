fb18d9eb842e1f5786d2b422f97a9e018ad5fe7e
Hussain Jamal <Hjamal9865@gmail.com>
2026-01-04 00:05:01 +0500

    working on auth

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { activateUser, loginUser, logoutUser, registrationUser } from '../controllers/user.controllers'
+import { isAuthenticated } from '../middlewares/auth'
 const userRouter = express.Router()
 
 userRouter.post('/registration' , registrationUser)
 userRouter.post('/activate-user' , activateUser)
 userRouter.post('/login' , loginUser)
-userRouter.post('/logout' , logoutUser)
+userRouter.post('/logout', isAuthenticated , logoutUser)
 
 export default userRouter
